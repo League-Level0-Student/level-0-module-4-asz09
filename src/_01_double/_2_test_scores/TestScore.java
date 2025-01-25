20b66e0a245c00d8bb927b81cd31c0d794737488
whale92130 <alexander92130@gmail.com>
2025-01-24 18:25:11 -0800

    Finished test Scores!

--- src/_01_double/_2_test_scores/TestScore.java
+++ src/_01_double/_2_test_scores/TestScore.java
@@ -4,14 +4,23 @@ import javax.swing.JOptionPane;
 
 public class TestScore {
 	public static void main(String[] args) {
-		JOptionPane.showInputDialog("What is your test score?");
+		int score = Integer.parseInt(JOptionPane.showInputDialog("What is your test score?"));
 		
-String input;
-if (input 80){
-JOptionPane.showMessageDialog(null,"Nice job!");	
-}
+		if (score >= 80){
+		JOptionPane.showMessageDialog(null,("Great!"));
+		}
+		else if(score >= 70) {
+			JOptionPane.showMessageDialog(null,("Not bad!!"));
+		}
+		else if (score<= 69) {
+			JOptionPane.showMessageDialog(null,("You can do this, good luck next time!"));
 		
-		
+		}
 	}
+
+
+	}
+
+
 	
-}
+	
