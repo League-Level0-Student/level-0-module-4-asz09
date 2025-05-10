a80e83f90ff211b005302e89bf40f4d2782b2a31
Aarit Jayaswal <aaritrj@gmail.com>
2025-05-09 18:26:16 -0700

    started stringmethods.

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,14 +5,16 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+String program = "one";
+System.out.println(program.charAt(2));
+		
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
 
-		
+	
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(program.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
@@ -25,6 +27,13 @@ public class StringMethods {
 		//             for char 'b', then print "b is at index 1"
 		
 	}
-}
+
+	
+	}
+
+	
+		// TODO Auto-generated method stub
+		
+	
 
 
